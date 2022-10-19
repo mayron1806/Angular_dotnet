596666a7012a8c2608b292cc0a51d30c9f24ce05
Mayron <mayronfernandes01@gmail.com>
2022-10-19 18:10:08 -0300

    eventos page

---
+++ Client/ProEventosApp/src/app/eventos/EventType.ts
@@ -0,0 +1,11 @@
+
+type Evento = {
+  id: number,
+  local: string,
+  dataEvento: string,
+  tema: string,
+  quantodadePessoas: number,
+  lote: string,
+  imagemURL: string
+}
+export default Evento;
--- Client/ProEventosApp/src/app/eventos/eventos.component.ts
+++ Client/ProEventosApp/src/app/eventos/eventos.component.ts
@@ -1,14 +1,44 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import Evento from "./EventType";
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
   styleUrls: ['./eventos.component.css']
 })
 export class EventosComponent implements OnInit {
+  carregandoEventos = true;
 
-  public eventos: any;
+  eventosFiltrados: Evento[] = []
+  eventos: Evento[] = [];
+  larguraImagem = 50;
+  mostrarImagens = true;
+
+  _filtro = "";
+
+
+
+  public get filtro() {
+    return this._filtro;
+  };
+  public set filtro(valor) {
+    this._filtro = valor;
+    this.eventosFiltrados = this.filtraEventos();
+  };
+
+  private filtraEventos() {
+    return this.eventos.filter(e => {
+      // deixa palavras do evento a serem filtradas em minusculo
+      const tema = e.tema.toLowerCase();
+      const local = e.local.toLowerCase();
+      // deixa filtro em minusculo
+      const filtro = this._filtro.toLowerCase();
+
+      if (tema.includes(filtro) || local.includes(filtro)) return e;
+      return;
+    });
+  }
+
   constructor(private http: HttpClient) {
   }
 
@@ -19,11 +49,14 @@ export class EventosComponent implements OnInit {
   public GetEventos(): any {
     this.http.get("https://localhost:5000/api/evento").subscribe(
       response => {
-        this.eventos = response;
-        console.log(response);
+        const novosEventos = response as Evento[];
+        this.eventos = novosEventos;
+        this.carregandoEventos = false;
+        this.eventosFiltrados = this.filtraEventos();
       },
       error => {
         console.log(error);
+        this.carregandoEventos = false;
       }
     );
   }
